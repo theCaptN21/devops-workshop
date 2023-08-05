56a98458213b70d0ac85ef0e3f3c5eaa13397592
theCaptN21 <k.hen.0246@gmail.com>
2023-08-05 15:21:38 -0400

    New Version 4 added

---
+++ terraform_code/V4-EC2-With_VPC_for_each.tf
@@ -0,0 +1,99 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "demo-server" {
+    ami = "ami-053b0d53c279acc90"
+    instance_type = "t2.micro"
+    key_name = "Workshop"
+    //security_groups = [ "demo-sg" ]
+    vpc_security_group_ids = [aws_security_group.demo-sg.id]
+    subnet_id = aws_subnet.kat-public-subnet-01.id
+    for_each = toset(["jenkins-master", "build-slave", "ansible"])
+    tags = {
+      Name = "${each.key}"
+    }
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH Access"
+  vpc_id = aws_vpc.kat-vpc.id
+
+  ingress {
+    description      = "SSH Access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ssh-port"
+  }
+}
+
+resource "aws_vpc" "kat-vpc" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+      Name = "kat-vpc"
+  }
+}
+
+resource "aws_subnet" "kat-public-subnet-01" {
+  vpc_id = aws_vpc.kat-vpc.id
+  cidr_block = "10.1.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "kat-public-subnet-01"
+  }
+}
+
+resource "aws_subnet" "kat-public-subnet-02" {
+  vpc_id = aws_vpc.kat-vpc.id
+  cidr_block = "10.1.2.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "kat-public-subnet-02"
+  }
+}
+
+resource "aws_internet_gateway" "kat-igw" {
+  vpc_id = aws_vpc.kat-vpc.id
+  tags = {
+    Name = "kat-igw"
+  }
+}
+
+resource "aws_route_table" "kat-public-rt" {
+  vpc_id = aws_vpc.kat-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.kat-igw.id
+  }
+}
+
+resource "aws_route_table_association" "kat-rta-public-subnet-01" {
+  subnet_id = aws_subnet.kat-public-subnet-01.id
+  route_table_id = aws_route_table.kat-public-rt.id 
+}
+
+resource "aws_route_table_association" "kat-rta-public-subnet-02" {
+  subnet_id = aws_subnet.kat-public-subnet-02.id
+  route_table_id = aws_route_table.kat-public-rt.id 
+}
+
+
+
+
+
