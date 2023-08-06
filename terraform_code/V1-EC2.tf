217d5e1703690bab223c2a0d0a7bd8f26912ec9b
theCaptN21 <k.hen.0246@gmail.com>
2023-08-06 11:42:07 -0400

    New Jenkins Slave File

--- terraform_code/V1-EC2.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_instance" "demo-server" {
-    ami = "ami-0f34c5ae932e6f0e4"
-    instance_type = "t2.micro"
-    key_name = "Workshop"
-}
